c1ca1ea9a53030dad22f8fd8a7875b424eca6e5b
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2022-02-06 16:27:26 +0100

    Update entrypoint to use python 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
     device-tree-compiler ca-certificates python3 python2
-ln -sf /usr/bin/python3 /usr/bin/python
+ln -sf /usr/bin/python2 /usr/bin/python
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
 if [[ $arch = "arm64" ]]; then
